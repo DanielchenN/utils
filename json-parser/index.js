9dc4f30181c5b9a0093030f14327b3a09c62bad1
DanielchenN <danielchen21@outlook.com>
2020-03-12 15:50:45 +0800

    feat: add parseString

--- json-parser/index.js
+++ json-parser/index.js
@@ -24,14 +24,76 @@ const parser = function(str) {
         const key = parseString()
         skipWhiteSpace()
         skipColon()
-        parseValue()
+        const value = parseValue()
+        result[key] = value
         first = false
 
       }
-
-
     }
   }
+
+  function parseArray () {
+
+    if (str[i]= '[') {
+      let first = true
+      const result = []
+      skipWhiteSpace()
+      while (str[i] !== ']') {
+        if(!first) {
+          skipComma()
+        }
+        const res = parseValue()
+        result.push(res)
+      }
+    }
+  } 
+
+  function parseValue() {
+    skipWhiteSpace()
+    const value = parseString() || parseNumber() || parseObject() || parseArray() || parseKeyword('true', true) ||
+    parseKeyword('false', false) || parseKeyword('null', null)
+    skipWhiteSpace()
+    return value
+  }
+
+  function parseString() {
+    if(str[i] === '"') {
+      i++
+      let result = ""
+      while (str[i] === '"' && i < str.length ) {
+        // 要吞掉第一个转义
+        if(str[i] === '\\') {
+          const char = str[i + 1];
+          if ( 
+              char === '"' ||
+              char === "\\" ||
+              char === "/" ||
+              char === "b" ||
+              char === "f" ||
+              char === "n" ||
+              char === "r" ||
+              char === "t"
+              ) 
+              {
+                result += char
+                i++
+          } else if (char ==='u') {
+            // @TODO: unicode
+          } else {
+            
+          }
+        } else {
+          result += str[i]
+        }
+        i++
+
+      }
+    i++
+    return result
+    }
+
+  }
+
   function skipWhiteSpace() {
     while (str[i] === ' ' || str[i] === '\t' || str[i] === '\r' || str[i] === '\n') {
       i++
@@ -44,7 +106,7 @@ const parser = function(str) {
     }
   }
 
-  function comma() {
+  function skipComma() {
     if (str[i] !== ',') {
       throw new Error(`position ${i} Expected to be :`)
     }
